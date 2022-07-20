de086215b1821136d90c4c9ee62b61851a008c22
ClementViricel <viricel.clement@gmail.com>
2022-07-20 11:34:40 +0200

    commit to change PC

--- application/main/routers/medg_document_matching.py
+++ application/main/routers/medg_document_matching.py
@@ -7,8 +7,8 @@ router = APIRouter(prefix="/medg_match")
 
 
 @router.get("/")
-async def question_classification(input_text: str, prescription: str):
-    question_type = medg_document_service.match(
+async def medg_document_route(input_text: str, prescription: str):
+    document_res = medg_document_service.match(
         input_text=input_text, prescription=prescription
     )
-    return question_type
+    return document_res
--- application/main/services/document_matching.py
+++ application/main/services/document_matching.py
@@ -1,34 +1,21 @@
+from application.main.src.models.data_models import MedGDocument
+from application.main.src.core.es_search import match_with_prescription
+
 from elasticsearch import Elasticsearch
 
 
 class MedGDocumentService:
     def __init__(self, env: str = "dev") -> None:
         self.es_client = Elasticsearch(
-            hosts="https://data-elastic-dev.360medics.com:9200",
+            hosts=f"https://data-elastic-{env}.360medics.com:9200",
             http_auth=("elastic", "david"),
             verify_certs=True,
         )
 
-    def match(self, input_text: str, prescription: str) -> str:
-        res = self.es_client.search(
-            index="tools-flat-content",
-            body={
-                "query": {
-                    "bool": {
-                        "must": [
-                            {"match": {"content": f"{input_text}"}},
-                            {"match": {"content": f"{prescription}"}},
-                        ]
-                    }
-                },
-                "_source": [
-                    "name",
-                    "source",
-                    "keywords.title",
-                    "keywords.description",
-                    "pdf_data.extracted_title",
-                    "",
-                ],
-            },
-        )
-        return res["hits"]["hits"]
+    def match(self, document: MedGDocument) -> str:
+        if document.input_text and document.prescription:
+            return match_with_prescription(
+                self.es_client, document.input_text, document.prescription
+            )
+        else:
+            return ""
