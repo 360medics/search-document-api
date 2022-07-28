29587129f2d29d6cd0f25911d4012a6625eab63f
ClementViricel <viricel.clement@gmail.com>
2022-07-28 17:08:36 +0200

    add search tool api

--- application/services/medg_document_matching.py
+++ application/services/medg_document_matching.py
@@ -1,9 +1,5 @@
-from application.src.core.search_api import get_medics
+from application.src.core.search_api import get_medics, get_search
 from application.src.models.data_models import MedGDocument
-from application.src.core.es_search import (
-    match_texts,
-    match_texts_with_prescriptions,
-)
 from application.src.core.preprocessing import split_text
 import logging
 
@@ -17,25 +13,20 @@ class MedGDocumentService:
         if Config.API_DEBUG_MODE:
             logger.setLevel(logging.DEBUG)
             self.test_patient = MedGDocument.parse_raw(
-                open("data/patient_8.json").read()
+                open("data/patient_5.json").read()
             )
 
     def match(self, document: MedGDocument, explain: bool = False) -> str:
         document = self.test_patient if self.test_patient else document
         consult = document.Consultations[-2]
         if consult.Resultat_consultation and consult.Prescription:
-            text = consult.Resultat_consultation
-            text = split_text(text)
+            texts = consult.Resultat_consultation
             prescriptions = split_text(consult.Prescription)
             medics_res = [get_medics(prescription) for prescription in prescriptions]
-            es_res = match_texts_with_prescriptions(
-                text, prescriptions, explain=explain
-            )
-            return medics_res + es_res
+            search_res = get_search(texts)
+            return medics_res + search_res
         elif consult.Resultat_consultation:
-            text = consult.Resultat_consultation
-            text = split_text(text)
-            return match_texts(text, explain=explain)
+            return get_search(consult.Resultat_consultation)
         elif consult.Prescription:
             prescriptions = split_text(consult.Prescription)
             return [get_medics(prescription) for prescription in prescriptions]
--- application/src/core/search_api.py
+++ application/src/core/search_api.py
@@ -1,26 +1,47 @@
 import requests
 import json
 from config import Config
+import logging
+
+logger = logging.getLogger("uvicorn.error")
 
 
-def get_search_all(text):
+def get_search(text):
     params = {
         "lang": "fr",
         "q": text,
         "medics_area": "ansm",
         "highlight": True,
         "offset": 0,
-        "limit": 1,
+        "limit": 10,
         "page": 1,
         "courtry": "FR",
         "skiprcppart": 0,
     }
-    resp = requests.post(f"{Config.baseURI}/v3/search/tools", params=params)
+    body = {
+        "body": {
+            "id": 223046,
+            "profession_id": 2,
+            "roles": ["ROLE_EXPERT", "ROLE_BETA_USER", "ROLE_USER"],
+            "specialty_id": 26,
+            "title": "DOCTOR",
+        }
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Token LCCDBS0TykGkGfY8UYGkBNnVS4AFqUOSavs3jVrN",
+        "X-User-Api-Key": "1ade9001222a075902bc1ea12e0dc643",
+    }
+    logger.info(f"{text}")
+    resp = requests.post(
+        f"{Config.baseURI}/v3/search/tool", params=params, json=body, headers=headers
+    )
     return json.loads(resp.content.decode("utf-8")).get("results")
 
 
 def get_medics(text):
     params = {"lang": "fr", "q": text, "medics_area": "ansm", "offset": 0, "limit": 1}
+    logger.info(text)
     resp = requests.post(
         f"{Config.MEDICS_URL_API}/rcp/v0",
         params=params,
